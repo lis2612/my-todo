e4a9161bdd13226ba46d94c4c2d2ecf36180c3f6
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-10-08 13:04:15 +0400

    refactoring cards

--- src/components/generic/Window.jsx
+++ src/components/generic/Window.jsx
@@ -3,7 +3,7 @@ import styles from "./Window.module.scss";
 import { Link } from "react-router-dom";
 
 function Window() {
-const [projects, setProjects] = useState([
+  const [projects, setProjects] = useState([
     {
       id: 1,
       title: "Pro1",
@@ -40,26 +40,25 @@ const [projects, setProjects] = useState([
       title: "Pro1",
       description: "Empty project",
     },
-  ])
+  ]);
 
   const createProject = () => {
     setProjects((prev) => [...prev, { id: projects.length, title: "new pro", description: "hghsd jhsdf jshdgf" }]);
-  }
+  };
 
   return (
     <>
       <div className={styles.container}>
         {projects.map((project) => {
           return (
-            <div className={styles.card}>
-              <Link
-                className={styles.card__link}
-                to={`/project/${project.id}`}>
+            <Link
+              className={styles.card}
+              to={`/project/${project.id}`}>
+              
                 <h3 className={styles.card__title}>{project.title}</h3>
-              </Link>
+                <p className={styles.card__description}>{project.description}</p>
 
-              <p className={styles.card__description}>{project.description}</p>
-            </div>
+            </Link>
           );
         })}
         <button
