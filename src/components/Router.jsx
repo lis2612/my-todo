03e7e3dce21eead571c0e0650f6cce5a8c2ec593
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-10-08 21:04:03 +0400

    add routing to tasks

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Projects from "./screens/Projects/Projects";
+import Tasks from "./screens/Tasks/Tasks";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          element={<Projects/>}
+          element={<Projects />}
           path="/"
+        />
+        <Route
+          element={<Tasks/>}
+          path="/projects/:id"
         />
 
         {/* Route for not found page */}
