a03a1a1315b1d2ad49960025f534ece9e1c800d9
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-10-10 16:38:52 +0400

    Peview version tasks page

--- src/components/screens/Tasks/Tasks.jsx
+++ src/components/screens/Tasks/Tasks.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./Tasks.module.scss";
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 
 function Tasks() {
   const { id } = useParams();
@@ -55,10 +56,77 @@ function Tasks() {
       status: "queue",
     },
   ]);
+
+  const statusList = ["queue", "development", "done"];
+
+  function DroppableColumn(props) {
+    const { setNodeRef } = useDroppable({
+      id: props.id,
+    });
+    return (
+      <div
+        ref={setNodeRef}
+        className={styles.canban__column}
+      id={props.id}>
+        <h2 className={styles.canban__columnTitle}>{props.id}</h2>
+        {props.children}
+      </div>
+    );
+  }
+
+  function DraggableCard(props) {
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+      id: props.task.id,
+    });
+    const style = transform
+      ? {
+          transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+          scale: "1.1",
+        }
+      : undefined;
+
+    return (
+      <div
+        className={styles.card}
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}>
+        <h2 className={styles.card__title}>{props.task.title}</h2>
+        <p>{props.task.description}</p>
+      </div>
+    );
+  }
+
+  const changeStatus = (id, status) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    const changedTask = tasks.filter((task) => task.id === id)[0];
+    changedTask.status = status;
+    setTasks([...newTasks, changedTask]);
+  };
+
   return (
     <>
       <div className={styles.canban}>
-        <div className={styles.canban__column}>
+        <DndContext onDragEnd={(e) => changeStatus(e.active.id, e.over.id)}>
+          {statusList.map((status) => (
+            <DroppableColumn id={status} key={status}>
+              {tasks
+                .filter((task) => task.projectId === +id)
+                .filter((task) => task.status === status)
+                .map((task) => {
+                  return (
+                    <DraggableCard
+                      task={task}
+                      key={task.id}
+                    />
+                  );
+                })}
+            </DroppableColumn>
+          ))}
+        </DndContext>
+
+        {/* <div className={styles.canban__column}>
           <h2 className={styles.canban__columnTitle}>Queue</h2>
           {tasks
             .filter((task) => task.projectId === +id)
@@ -101,7 +169,7 @@ function Tasks() {
                 </div>
               );
             })}
-        </div>
+        </div> */}
       </div>
     </>
   );
